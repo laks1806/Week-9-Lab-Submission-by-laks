4348f7f38195b6fb74506288e5ff369d8ed0d9b2
Lakshita Singh <67590424+laks1806@users.noreply.github.com>
2023-11-29 23:12:13 +0000

    log created

--- logger.py
+++ logger.py
@@ -1,21 +1,15 @@
 import csv
-import os
 import datetime
-import pandas as pd
+
 
 class TicTacToeLogger:
+
     def __init__(self):
-        self.log_directory = "./logs"
-        self.log_file_path = os.path.join(self.log_directory, "game_data.csv")
-        self.create_log_directory()
+        self.log_file_path = "game_data.csv"
         self.create_log_file()
 
-    def create_log_directory(self):
-        if not os.path.exists(self.log_directory):
-            os.makedirs(self.log_directory)
-
     def create_log_file(self):
-        if not os.path.isfile(self.log_file_path):
+        if not os.path.exists(self.log_file_path):
             with open(self.log_file_path, 'w', newline='') as file:
                 writer = csv.writer(file)
                 writer.writerow(["Game ID", "Winner", "Player 1", "Player 2", "Game Duration", "Timestamp"])
@@ -33,5 +27,6 @@ class TicTacToeLogger:
         self.append_to_csv(game_data)
 
     def append_to_csv(self, game_data):
-        df = pd.DataFrame(game_data, index=[0])
-        df.to_csv(self.log_file_path, mode='a', header=False, index=False)
+        with open(self.log_file_path, 'a', newline='') as file:
+            writer = csv.DictWriter(file, fieldnames=game_data.keys())
+            writer.writerow(game_data)
